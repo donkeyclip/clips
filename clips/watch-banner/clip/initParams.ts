1f6aa0726246d7f702ea679eea78607ffc8db2a2
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2023-06-26 17:30:52 +0300

    create watch-banner

---
+++ clips/watch-banner/clip/initParams.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: "Deafaut",
+    value: {
+      primaryColor: "#2B45A4",
+      secondaryColor: "#fff",
+      backgroundColor: "#324073",
+      fontColor: "#fff",
+      text: "Summer offers",
+      offer: ["35", "%", "off"],
+      cta: "Explore Now",
+      link: "https://donkeyclip.com/explore",
+      products: [
+        {
+          name: "Smartwatch",
+          image:
+            "https://donkeyclip.github.io/clips/clips/tech-banner/images/watch.png",
+        },
+        {
+          name: "Smartphone",
+          image:
+            "https://donkeyclip.github.io/clips/clips/tech-banner/images/phone.png",
+        },
+        {
+          name: "Laptop",
+          image:
+            "https://donkeyclip.github.io/clips/clips/tech-banner/images/laptop.png",
+        },
+      ],
+    },
+  },
+];
