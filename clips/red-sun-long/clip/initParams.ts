01114f7fca99b5698ff78a02a60555c5b31883b3
ivanvl01 <ivan.v@kissmybutton.gr>
2023-07-03 15:45:24 +0300

    create new clip

---
+++ clips/red-sun-long/clip/initParams.ts
@@ -0,0 +1,24 @@
+export default [
+  {
+    name: "Deafaut",
+    value: {
+      primaryColor: "#952020",
+      backgroundColor: "#F5F5F5",
+
+      fontColor: "#19181C",
+      text: "New Arrivals",
+      secondaryText: "Summer offers",
+      offer: "50% OFF",
+      cta: "Explore Now",
+      url: "https://www.google.com",
+      products: [
+        {
+          image: "https://i.ibb.co/Q9ff99H/smartwatch.png",
+        },
+        {
+          image: "https://i.ibb.co/Q9ff99H/smartwatch.png",
+        },
+      ],
+    },
+  },
+];
