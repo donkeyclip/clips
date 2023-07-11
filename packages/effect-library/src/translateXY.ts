c8b525bcd28cff51e5a6dff5781d06a1574de91e
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2023-07-11 12:28:05 +0300

    create grid banner

---
+++ packages/effect-library/src/translateXY.ts
@@ -0,0 +1,29 @@
+import { CSSEffect } from "@donkeyclip/motorcortex";
+export const translateXY = (
+  selector: string,
+  duration: number,
+  x: string,
+  initialX: string,
+  y: string,
+  initialY: string,
+) =>
+  new CSSEffect(
+    {
+      animatedAttrs: {
+        transform: {
+          translateX: x,
+          translateY: y,
+        },
+      },
+      initialValues: {
+        transform: {
+          translateX: initialX,
+          translateY: initialY,
+        },
+      },
+    },
+    {
+      selector,
+      duration,
+    },
+  );
