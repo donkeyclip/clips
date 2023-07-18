37fc473e60824d5bf5a9202556b37aa36f045344
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2023-07-18 22:50:44 +0300

    create watch banner clip

---
+++ clips/watch-banner/clip/clip.ts
@@ -0,0 +1,63 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+import { top, topMoveCombo } from "@donkeyclip/effects";
+import { HTMLClip } from "@donkeyclip/motorcortex";
+import { renderDonkeyclip } from "@donkeyclip/server";
+import pkg from "../package.json";
+import { clipIncidents } from "./ClipIncidents";
+import css from "./clip.css";
+import html from "./clip.html";
+import initParams from "./initParams";
+
+const host = document.getElementById("clip");
+
+if (!host)
+  throw new Error(
+    "No element with id 'clip' found, so can't show the clip! Please check your html",
+  );
+
+const clip = new HTMLClip({
+  html,
+  css,
+  host,
+  initParams: initParams[0].value,
+  containerParams: {
+    width: "300px",
+    height: "300px",
+  },
+  fonts: [
+    {
+      type: "google-font",
+      src: "https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Inter:wght@100;900&display=swap",
+    },
+  ],
+});
+
+clip.addIncident(
+  top({
+    value: "0%",
+    initValue: "-100%",
+    selector: ".text-wrapper .text",
+    duration: 400,
+    easing: "easeOutQuad",
+  }),
+  0,
+);
+clip.addIncident(
+  top({
+    value: "80%",
+    initValue: "100%",
+    selector: ".cta",
+    duration: 600,
+    easing: "easeOutQuad",
+  }),
+  0,
+);
+
+clip.addIncident(
+  topMoveCombo(".image", "0%", "100%", "-100%", 1000, 800, 3000),
+  0,
+);
+
+clip.addIncident(clipIncidents(".pr-name"), 0);
+
+export default renderDonkeyclip({ clipId: pkg.id, initParams, clip });
