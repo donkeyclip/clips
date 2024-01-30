04974c775d36c210d7569c2cbbc901d352ff7079
Samuel KMB <samuelkmb@Samuels-Macbook.local>
2024-01-30 15:54:39 +0200

    feat: added scale animation to 3 boxes

--- clips/summer-offers/clip/clip.ts
+++ clips/summer-offers/clip/clip.ts
@@ -68,119 +68,17 @@ clip.addIncident(top(".subtitle-inner-wrapper .letter", "50px", "0px"), 300);
     ),
     1500,
   );
+  clip.addIncident(
+    scaleSmallBigCombo(
+      ".box-2, .box-3, .box-4",
+      enterScenePosition - 1000,
+      exitScenePosition - 1000,
+    ),
+    2000,
+  );
 });
 
 // the clips that depend on the 'clip' should be placed at the bottom since they need the calculated duration of the whole clip
 clip.addIncident(pulsingGridDotsCombo(".dot", clip), 0);
-// clip.addIncident(scaleSmallBigCombo(".discount-wrapper"), 0);
-
-// clip.addIncident(
-//   top({
-//     value: "563px",
-//     initValue: "480px",
-//     selector: ".cursor",
-//     duration: 1000,
-//   }),
-//   0,
-// );
-// clip.addIncident(
-//   left({
-//     value: "190px",
-//     initValue: "1280px",
-//     selector: ".cursor",
-//     duration: 1000,
-//   }),
-//   0,
-// );
-// clip.addIncident(clipIncidents(".second-slide"), 1000);
-// clip.addIncident(
-//   opacity({ value: 1, initValue: 0, selector: "#slide-0", duration: 10 }),
-//   6889,
-// );
-// clip.addIncident(clipIncidents("#slide-0"), 6899);
-// clip.addIncident(
-//   opacity({ value: 1, initValue: 0, selector: "#slide-1", duration: 10 }),
-//   12000,
-// );
-// clip.addIncident(clipIncidents("#slide-1"), 12000);
-// clip.addIncident(
-//   opacity({ value: 1, initValue: 0, selector: "#slide-2", duration: 10 }),
-//   16790,
-// );
-// clip.addIncident(clipIncidents("#slide-2"), 16790);
-// clip.addIncident(
-//   top({
-//     value: "100%",
-//     initValue: "0%",
-//     selector: ".first-title-wrapper .latter",
-//     duration: 300,
-//     delay: "@stagger(0,300)",
-//   }),
-//   5600,
-// );
-// clip.addIncident(
-//   top({
-//     value: "0%",
-//     initValue: "100%",
-//     selector: ".second-title-wrapper .latter",
-//     duration: 300,
-//     delay: "@stagger(0,300)",
-//   }),
-//   6299,
-// );
-// clip.addIncident(
-//   left({
-//     value: "65%",
-//     initValue: "100%",
-//     selector: ".client-list-wrapper",
-//     duration: 500,
-//   }),
-//   6270,
-// );
-// clip.addIncident(
-//   backgroundColor({
-//     value: "#565454",
-//     initValue: "#1B1B1B",
-//     selector: "#card-0",
-//     duration: 200,
-//   }),
-//   6270,
-// );
-// clip.addIncident(
-//   backgroundColor({
-//     value: "#1B1B1B",
-//     initValue: "#565454",
-//     selector: "#card-0",
-//     duration: 200,
-//   }),
-//   12000,
-// );
-// clip.addIncident(
-//   backgroundColor({
-//     value: "#565454",
-//     initValue: "#1B1B1B",
-//     selector: "#card-1",
-//     duration: 200,
-//   }),
-//   12000,
-// );
-// clip.addIncident(
-//   backgroundColor({
-//     value: "#1B1B1B",
-//     initValue: "#565454",
-//     selector: "#card-1",
-//     duration: 200,
-//   }),
-//   16790,
-// );
-// clip.addIncident(
-//   backgroundColor({
-//     value: "#565454",
-//     initValue: "#1B1B1B",
-//     selector: "#card-2",
-//     duration: 200,
-//   }),
-//   16790,
-// );
 
 export default renderDonkeyclip({ clipId: pkg.id, initParams, clip });
