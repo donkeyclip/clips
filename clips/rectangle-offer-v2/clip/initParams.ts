e42b98ba24d484c32c3003d5ecd442107819a170
Samuel KMB <samuelkmb@Samuels-Macbook.local>
2024-02-01 15:48:46 +0200

    styles: WIP in layout

--- clips/rectangle-offer-v2/clip/initParams.ts
+++ clips/rectangle-offer-v2/clip/initParams.ts
@@ -11,7 +11,8 @@ export default [
 
       products: [
         {
-          description: "Description 1",
+          description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate nisi inventore est",
           featured_image:
             "https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
