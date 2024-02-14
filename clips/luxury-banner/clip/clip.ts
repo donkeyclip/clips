62f5ad4ee6e4ee6a972173278c3f15ad906b4308
Samuel KMB <samuelkmb@Samuels-Macbook.local>
2024-02-14 15:22:48 +0200

    feat: initial commit of new clip

--- clips/luxury-banner/clip/clip.ts
+++ clips/luxury-banner/clip/clip.ts
@@ -6,12 +6,6 @@ import css from "./clip.css";
 import html from "./clip.html";
 import initParams from "./initParams";
 import initParamsValidationRules from "./initParamsValidationRules";
-import {
-  clipPath,
-  clipPathExpandFromLineCombo,
-  color,
-  opacity,
-} from "./clipIncidents";
 
 const element = document.getElementById("clip");
 
@@ -27,8 +21,8 @@ const clip = new HTMLClip({
   initParamsValidationRules,
   initParams: initParams[0].value,
   containerParams: {
-    width: "300px",
-    height: "300px",
+    width: "705px",
+    height: "290px",
   },
   fonts: [
     {
@@ -40,247 +34,231 @@ const clip = new HTMLClip({
 
 const productsLength = initParams[0].value.products.length;
 
-const animationDuration = 1300;
-const displayImageDuration = 6000;
-const delayBetweenImageChange = 200;
-const animationDurationOfCombos = 700;
-const animationDurationOfStartingTextOverlaysAndHorizontalLine = 600;
+// const animationDuration = 1300;
+// const displayImageDuration = 6000;
+// const delayBetweenImageChange = 200;
+// const animationDurationOfCombos = 700;
+// const animationDurationOfStartingTextOverlaysAndHorizontalLine = 600;
 
-const animateClipStart = () => {
-  const startingPositionOfImageRightToLeftAnimation =
-    0 * (animationDuration + displayImageDuration + delayBetweenImageChange);
-  clip.addIncident(
-    opacity({
-      selector: `svg`,
-      from: 0,
-      to: 1,
-      duration: 1500,
-      easing: "easeInOutCubic",
-    }),
-    0,
-  );
+// const animateClipStart = () => {
+// const startingPositionOfImageRightToLeftAnimation =
+//   0 * (animationDuration + displayImageDuration + delayBetweenImageChange);
+// clip.addIncident(
+//   opacity({
+//     selector: `svg`,
+//     from: 0,
+//     to: 1,
+//     duration: 1500,
+//     easing: "easeInOutCubic",
+//   }),
+//   0
+// );
+// clip.addIncident(
+//   clipPath({
+//     selector: `.title-overlay, .subtitle-overlay`,
+//     from: "inset(100% 0% 0% 0%)",
+//     to: "inset(0% 0% 0% 0%)",
+//     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+//     easing: "easeInOutCubic",
+//   }),
+//   startingPositionOfImageRightToLeftAnimation + animationDurationOfCombos
+// );
+// clip.addIncident(
+//   color({
+//     selector: `.title, .subtitle`,
+//     from: "transparent",
+//     to: "var(--fontColor)",
+//     duration: 50,
+//   }),
+//   startingPositionOfImageRightToLeftAnimation + animationDuration + 50
+// );
+// clip.addIncident(
+//   clipPath({
+//     selector: `.title-overlay, .subtitle-overlay`,
+//     from: "inset(0% 0% 0% 0%)",
+//     to: "inset(0% 100% 0% 0%)",
+//     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+//     easing: "easeInOutCubic",
+//   }),
+//   startingPositionOfImageRightToLeftAnimation + animationDuration
+// );
+// clip.addIncident(
+//   clipPath({
+//     selector: `.horizontal-line`,
+//     from: "inset(0% 100% 0% 0%)",
+//     to: "inset(0% 0% 0% 0%)",
+//     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+//     easing: "easeInOutCubic",
+//   }),
+//   startingPositionOfImageRightToLeftAnimation
+// );
+// clip.addIncident(
+//   clipPath({
+//     selector: `.cta`,
+//     from: "inset(0% 100% 0% 0%)",
+//     to: "inset(0% 0% 0% 0%)",
+//     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+//     easing: "easeInOutCubic",
+//   }),
+//   startingPositionOfImageRightToLeftAnimation
+// );
+// };
 
-  clip.addIncident(
-    clipPath({
-      selector: `.title-overlay, .subtitle-overlay`,
-      from: "inset(100% 0% 0% 0%)",
-      to: "inset(0% 0% 0% 0%)",
-      duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-      easing: "easeInOutCubic",
-    }),
-    startingPositionOfImageRightToLeftAnimation + animationDurationOfCombos,
-  );
+// const animateTextOverlays = (
+// endingPosition: number,
+// duration: number,
+// index: number
+// ) => {
+// if (index === productsLength - 1) return;
+// SLIDE OVERLAY FROM BOTTOM TO TOP
+// clip.addIncident(
+//   clipPath({
+//     selector: `.title-overlay-2, .subtitle-overlay-2`,
+//     from: "inset(100% 0% 0% 0%)",
+//     to: "inset(0% 0% 0% 0%)",
+//     duration,
+//     easing: "easeInOutCubic",
+//   }),
+//   endingPosition
+// );
+// SLIDE OVERLAY FROM RIGHT TO LEFT
+// clip.addIncident(
+//   clipPath({
+//     selector: `.title-overlay-2, .subtitle-overlay-2`,
+//     from: "inset(0% 0% 0% 0%)",
+//     to: "inset(0% 100% 0% 0%)",
+//     duration,
+//     easing: "easeInOutCubic",
+//   }),
+//   endingPosition + duration
+// );
+// };
 
-  clip.addIncident(
-    color({
-      selector: `.title, .subtitle`,
-      from: "transparent",
-      to: "var(--fontColor)",
-      duration: 50,
-    }),
-    startingPositionOfImageRightToLeftAnimation + animationDuration + 50,
-  );
+// const animateImages = () =>
+// index: number,
+// animationDurationOfCombos: number,
+// startingPosition: number,
+// endingPosition: number
+// {
+// const initialImagePath = "inset(0% 0% 95% 100%)";
+// const endingImagePath = "inset(0% 0% 95% 0%)";
+// const initialImagePath2 = "inset(0% 0% 95% 0%)";
+// const endingImagePath2 = "inset(0% 0% -1% 0%)";
+// clip.addIncident(
+//   clipPathExpandFromLineCombo({
+//     selector: `#product-${index} img`,
+//     duration: animationDurationOfCombos,
+//     startingPosition,
+//     endingPosition,
+//     initialPath: initialImagePath,
+//     endingPath: endingImagePath,
+//     initialPath2: initialImagePath2,
+//     endingPath2: endingImagePath2,
+//     easing: "easeInOutCubic",
+//   }),
+//   0,
+// );
+// const showShadowAnimationDelay = 400;
+// const animationDurationOfImageShadow = 1000;
+// clip.addIncident(
+//   opacity({
+//     selector: `.img-overlay`,
+//     from: 0,
+//     to: 1,
+//     duration: animationDurationOfImageShadow,
+//     easing: "easeInOutCubic",
+//   }),
+//   startingPosition + animationDurationOfCombos + showShadowAnimationDelay
+// );
+// clip.addIncident(
+//   opacity({
+//     selector: `.img-overlay`,
+//     from: 1,
+//     to: 0,
+//     duration: animationDurationOfImageShadow,
+//     easing: "easeInOutCubic",
+//   }),
+//   endingPosition - animationDurationOfCombos - showShadowAnimationDelay
+// );
+// };
 
-  clip.addIncident(
-    clipPath({
-      selector: `.title-overlay, .subtitle-overlay`,
-      from: "inset(0% 0% 0% 0%)",
-      to: "inset(0% 100% 0% 0%)",
-      duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-      easing: "easeInOutCubic",
-    }),
-    startingPositionOfImageRightToLeftAnimation + animationDuration,
-  );
-
-  clip.addIncident(
-    clipPath({
-      selector: `.horizontal-line`,
-      from: "inset(0% 100% 0% 0%)",
-      to: "inset(0% 0% 0% 0%)",
-      duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-      easing: "easeInOutCubic",
-    }),
-    startingPositionOfImageRightToLeftAnimation,
-  );
-
-  clip.addIncident(
-    clipPath({
-      selector: `.cta`,
-      from: "inset(0% 100% 0% 0%)",
-      to: "inset(0% 0% 0% 0%)",
-      duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-      easing: "easeInOutCubic",
-    }),
-    startingPositionOfImageRightToLeftAnimation,
-  );
-};
-
-const animateTextOverlays = (
-  endingPosition: number,
-  duration: number,
-  index: number,
-) => {
-  if (index === productsLength - 1) return;
-  // SLIDE OVERLAY FROM BOTTOM TO TOP
-  clip.addIncident(
-    clipPath({
-      selector: `.title-overlay-2, .subtitle-overlay-2`,
-      from: "inset(100% 0% 0% 0%)",
-      to: "inset(0% 0% 0% 0%)",
-      duration,
-      easing: "easeInOutCubic",
-    }),
-    endingPosition,
-  );
-  // SLIDE OVERLAY FROM RIGHT TO LEFT
-  clip.addIncident(
-    clipPath({
-      selector: `.title-overlay-2, .subtitle-overlay-2`,
-      from: "inset(0% 0% 0% 0%)",
-      to: "inset(0% 100% 0% 0%)",
-      duration,
-      easing: "easeInOutCubic",
-    }),
-    endingPosition + duration,
-  );
-};
-
-const animateImages = (
-  index: number,
-  animationDurationOfCombos: number,
-  startingPosition: number,
-  endingPosition: number,
-) => {
-  const initialImagePath = "inset(0% 0% 95% 100%)";
-  const endingImagePath = "inset(0% 0% 95% 0%)";
-
-  const initialImagePath2 = "inset(0% 0% 95% 0%)";
-  const endingImagePath2 = "inset(0% 0% -1% 0%)";
-
-  clip.addIncident(
-    clipPathExpandFromLineCombo({
-      selector: `#product-${index} img`,
-      duration: animationDurationOfCombos,
-      startingPosition,
-      endingPosition,
-      initialPath: initialImagePath,
-      endingPath: endingImagePath,
-      initialPath2: initialImagePath2,
-      endingPath2: endingImagePath2,
-      easing: "easeInOutCubic",
-    }),
-    0,
-  );
-
-  const showShadowAnimationDelay = 400;
-  const animationDurationOfImageShadow = 1000;
-
-  clip.addIncident(
-    opacity({
-      selector: `.img-overlay`,
-      from: 0,
-      to: 1,
-      duration: animationDurationOfImageShadow,
-      easing: "easeInOutCubic",
-    }),
-    startingPosition + animationDurationOfCombos + showShadowAnimationDelay,
-  );
-
-  clip.addIncident(
-    opacity({
-      selector: `.img-overlay`,
-      from: 1,
-      to: 0,
-      duration: animationDurationOfImageShadow,
-      easing: "easeInOutCubic",
-    }),
-    endingPosition - animationDurationOfCombos - showShadowAnimationDelay,
-  );
-};
-
-animateClipStart();
+// animateClipStart();
 
 Array.from({ length: productsLength }).forEach((_, index) => {
-  const startingPosition =
-    index *
-    (animationDuration + displayImageDuration + delayBetweenImageChange);
-  const endingPosition = startingPosition + displayImageDuration;
+  // const startingPosition =
+  //   index *
+  //   (animationDuration + displayImageDuration + delayBetweenImageChange);
+  // const endingPosition = startingPosition + displayImageDuration;
 
-  animateImages(
-    index,
-    animationDurationOfCombos,
-    startingPosition,
-    endingPosition,
-  );
-  animateTextOverlays(endingPosition, animationDurationOfCombos, index);
+  // animateImages(
+  //   index,
+  //   animationDurationOfCombos,
+  //   startingPosition,
+  //   endingPosition
+  // );
+  // animateTextOverlays(endingPosition, animationDurationOfCombos, index);
 
   if (index === productsLength - 1) {
     // RESET ANIMATIONS TO INITIAL STATE
-
-    clip.addIncident(
-      opacity({
-        selector: `svg`,
-        from: 1,
-        to: 0,
-        duration: 700,
-        easing: "easeInOutCubic",
-      }),
-      endingPosition + animationDurationOfCombos,
-    );
-
-    clip.addIncident(
-      clipPath({
-        selector: `.horizontal-line`,
-        from: "inset(0% 0% 0% 0%)",
-        to: "inset(0% 100% 0% 0%)",
-        duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-        easing: "easeInOutCubic",
-      }),
-      endingPosition + animationDurationOfCombos,
-    );
-
-    clip.addIncident(
-      clipPath({
-        selector: `.title-overlay, .subtitle-overlay`,
-        from: "inset(100% 0% 0% 0%)",
-        to: "inset(0% 0% 0% 0%)",
-        duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-        easing: "easeInOutCubic",
-      }),
-      endingPosition,
-    );
-
-    clip.addIncident(
-      color({
-        selector: `.title, .subtitle`,
-        from: "var(--fontColor)",
-        to: "transparent",
-        duration: 50,
-      }),
-      endingPosition + animationDurationOfCombos,
-    );
-
-    clip.addIncident(
-      clipPath({
-        selector: `.title-overlay, .subtitle-overlay`,
-        from: "inset(0% 0% 0% 0%)",
-        to: "inset(0% 100% 0% 0%)",
-        duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-        easing: "easeInOutCubic",
-      }),
-      endingPosition + animationDurationOfStartingTextOverlaysAndHorizontalLine,
-    );
-
-    clip.addIncident(
-      clipPath({
-        selector: `.cta`,
-        from: "inset(0% 0% 0% 0%)",
-        to: "inset(0% 100% 0% 0%)",
-        duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-        easing: "easeInOutCubic",
-      }),
-      endingPosition + animationDurationOfCombos,
-    );
+    // clip.addIncident(
+    //   opacity({
+    //     selector: `svg`,
+    //     from: 1,
+    //     to: 0,
+    //     duration: 700,
+    //     easing: "easeInOutCubic",
+    //   }),
+    //   endingPosition + animationDurationOfCombos
+    // );
+    // clip.addIncident(
+    //   clipPath({
+    //     selector: `.horizontal-line`,
+    //     from: "inset(0% 0% 0% 0%)",
+    //     to: "inset(0% 100% 0% 0%)",
+    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+    //     easing: "easeInOutCubic",
+    //   }),
+    //   endingPosition + animationDurationOfCombos
+    // );
+    // clip.addIncident(
+    //   clipPath({
+    //     selector: `.title-overlay, .subtitle-overlay`,
+    //     from: "inset(100% 0% 0% 0%)",
+    //     to: "inset(0% 0% 0% 0%)",
+    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+    //     easing: "easeInOutCubic",
+    //   }),
+    //   endingPosition
+    // );
+    // clip.addIncident(
+    //   color({
+    //     selector: `.title, .subtitle`,
+    //     from: "var(--fontColor)",
+    //     to: "transparent",
+    //     duration: 50,
+    //   }),
+    //   endingPosition + animationDurationOfCombos
+    // );
+    // clip.addIncident(
+    //   clipPath({
+    //     selector: `.title-overlay, .subtitle-overlay`,
+    //     from: "inset(0% 0% 0% 0%)",
+    //     to: "inset(0% 100% 0% 0%)",
+    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+    //     easing: "easeInOutCubic",
+    //   }),
+    //   endingPosition + animationDurationOfStartingTextOverlaysAndHorizontalLine
+    // );
+    // clip.addIncident(
+    //   clipPath({
+    //     selector: `.cta`,
+    //     from: "inset(0% 0% 0% 0%)",
+    //     to: "inset(0% 100% 0% 0%)",
+    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
+    //     easing: "easeInOutCubic",
+    //   }),
+    //   endingPosition + animationDurationOfCombos
+    // );
   }
 });
 
--- clips/luxury-banner/clip/initParams.ts
+++ clips/luxury-banner/clip/initParams.ts
@@ -2,31 +2,30 @@ export default [
   {
     name: "Luxury Banner",
     value: {
-      title: "Summer Offers",
-      subtitle: "Save Up To",
-      text: "50% OFF",
-      secondaryText: "Hand - Made - Hand - Made - Hand - Made - ",
+      title: "Summer",
+      subtitle: "Offers",
+      text: "50%",
+      secondaryText: "OFF",
+      ctaText: "Explore Now",
 
       fontColor: "#595959",
-      primaryColor: "#DADADA",
-      secondaryColor: "#EEE7E0",
+      primaryColor: "#272727",
+      secondaryColor: "#F30C38",
       backgroundColor: "#EFEFEF",
-
-      ctaText: "Explore Now",
 
       products: [
         {
           featured_image:
             "https://images.unsplash.com/photo-1589363358751-ab05797e5629?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
-        {
-          featured_image:
-            "https://images.unsplash.com/photo-1623998021446-45cd9b269056?q=80&w=2079&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-        {
-          featured_image:
-            "https://images.unsplash.com/photo-1511370235399-1802cae1d32f?q=80&w=2055&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
+        // {
+        //   featured_image:
+        //     "https://images.unsplash.com/photo-1623998021446-45cd9b269056?q=80&w=2079&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        // },
+        // {
+        //   featured_image:
+        //     "https://images.unsplash.com/photo-1511370235399-1802cae1d32f?q=80&w=2055&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        // },
       ],
     },
   },
