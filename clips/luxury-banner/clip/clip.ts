fdd50a4ef190e164ca7aee344dd616125fa8f360
Samuel KMB <samuelkmb@Samuels-Macbook.local>
2024-02-15 16:18:34 +0200

    refactor: cleaned unused code

--- clips/luxury-banner/clip/clip.ts
+++ clips/luxury-banner/clip/clip.ts
@@ -6,7 +6,7 @@ import css from "./clip.css";
 import html from "./clip.html";
 import initParams from "./initParams";
 import initParamsValidationRules from "./initParamsValidationRules";
-import { strokeDashOffset } from "./clipIncidents";
+import { clipPath, strokeDashOffset } from "./clipIncidents";
 
 const element = document.getElementById("clip");
 
@@ -35,13 +35,26 @@ const clip = new HTMLClip({
 
 const productsLength = initParams[0].value.products.length;
 
-// const animationDuration = 1300;
+const animationDurationOfCircles = 1300;
 // const displayImageDuration = 6000;
 // const delayBetweenImageChange = 200;
 // const animationDurationOfCombos = 700;
 // const animationDurationOfStartingTextOverlaysAndHorizontalLine = 600;
 
-const animateCircles = () => {
+const animateText = (delayBeforeStart = 0) => {
+  clip.addIncident(
+    clipPath({
+      selector: `.title, .subtitle, .cta`,
+      from: "inset(100% 0% 0% 0%)",
+      to: "inset(0% 0% 0% 0%)",
+      duration: animationDurationOfCircles,
+      easing: "easeInOutCubic",
+    }),
+    delayBeforeStart,
+  );
+};
+
+const animateCircles = (delayBeforeStart = 0) => {
   const radiusOfCircle = 128.5;
   const pi = 3.14;
   const circumferenceOfCircle = Math.round(2 * pi * radiusOfCircle) + 1;
@@ -56,7 +69,7 @@ const animateCircles = () => {
       duration: 1500,
       easing: "easeInOutCubic",
     }),
-    0,
+    delayBeforeStart,
   );
   // PRODUCT IMAGE CIRCLE
   clip.addIncident(
@@ -69,7 +82,7 @@ const animateCircles = () => {
       duration: 1500,
       easing: "easeInOutCubic",
     }),
-    0,
+    delayBeforeStart,
   );
   // CORNER CIRCLE
   clip.addIncident(
@@ -82,166 +95,48 @@ const animateCircles = () => {
       duration: 1000,
       easing: "easeInOutCubic",
     }),
-    0,
+    delayBeforeStart,
   );
 };
 
-// const animateTextOverlays = (
-// endingPosition: number,
-// duration: number,
-// index: number
-// ) => {
-// if (index === productsLength - 1) return;
-// SLIDE OVERLAY FROM BOTTOM TO TOP
-// clip.addIncident(
-//   clipPath({
-//     selector: `.title-overlay-2, .subtitle-overlay-2`,
-//     from: "inset(100% 0% 0% 0%)",
-//     to: "inset(0% 0% 0% 0%)",
-//     duration,
-//     easing: "easeInOutCubic",
-//   }),
-//   endingPosition
-// );
-// SLIDE OVERLAY FROM RIGHT TO LEFT
-// clip.addIncident(
-//   clipPath({
-//     selector: `.title-overlay-2, .subtitle-overlay-2`,
-//     from: "inset(0% 0% 0% 0%)",
-//     to: "inset(0% 100% 0% 0%)",
-//     duration,
-//     easing: "easeInOutCubic",
-//   }),
-//   endingPosition + duration
-// );
-// };
+const animateProductImages = (delayBeforeStart = 0) => {
+  Array.from({ length: productsLength }).forEach((_, index) => {
+    const initialDelay = index === 0 ? delayBeforeStart : 0;
+    const animationDuration = 1300;
+    const displayImageDuration = 4000;
+    const delayBetweenImageChange = -1450;
+    const startingPosition =
+      index *
+      (animationDuration + displayImageDuration + delayBetweenImageChange);
+    const endingPosition = startingPosition + displayImageDuration;
 
-// const animateImages = () =>
-// index: number,
-// animationDurationOfCombos: number,
-// startingPosition: number,
-// endingPosition: number
-// {
-// const initialImagePath = "inset(0% 0% 95% 100%)";
-// const endingImagePath = "inset(0% 0% 95% 0%)";
-// const initialImagePath2 = "inset(0% 0% 95% 0%)";
-// const endingImagePath2 = "inset(0% 0% -1% 0%)";
-// clip.addIncident(
-//   clipPathExpandFromLineCombo({
-//     selector: `#product-${index} img`,
-//     duration: animationDurationOfCombos,
-//     startingPosition,
-//     endingPosition,
-//     initialPath: initialImagePath,
-//     endingPath: endingImagePath,
-//     initialPath2: initialImagePath2,
-//     endingPath2: endingImagePath2,
-//     easing: "easeInOutCubic",
-//   }),
-//   0,
-// );
-// const showShadowAnimationDelay = 400;
-// const animationDurationOfImageShadow = 1000;
-// clip.addIncident(
-//   opacity({
-//     selector: `.img-overlay`,
-//     from: 0,
-//     to: 1,
-//     duration: animationDurationOfImageShadow,
-//     easing: "easeInOutCubic",
-//   }),
-//   startingPosition + animationDurationOfCombos + showShadowAnimationDelay
-// );
-// clip.addIncident(
-//   opacity({
-//     selector: `.img-overlay`,
-//     from: 1,
-//     to: 0,
-//     duration: animationDurationOfImageShadow,
-//     easing: "easeInOutCubic",
-//   }),
-//   endingPosition - animationDurationOfCombos - showShadowAnimationDelay
-// );
-// };
+    clip.addIncident(
+      clipPath({
+        selector: `#product-${index} img`,
+        from: "inset(0% 0% 100% 0%)",
+        to: "inset(0% 0% 0% 0%)",
+        duration: animationDuration + 500,
+        delay: index === 0 ? initialDelay + 200 : 200,
+        easing: "easeInOutCubic",
+      }),
+      startingPosition,
+    );
+
+    clip.addIncident(
+      clipPath({
+        selector: `#product-${index} img`,
+        from: "inset(0% 0% 0% 0%)",
+        to: "inset(100% 0% 0% 0%)",
+        duration: animationDuration + 600,
+        easing: "easeOutCubic",
+      }),
+      endingPosition,
+    );
+  });
+};
 
 animateCircles();
-
-Array.from({ length: productsLength }).forEach((_, index) => {
-  // const startingPosition =
-  //   index *
-  //   (animationDuration + displayImageDuration + delayBetweenImageChange);
-  // const endingPosition = startingPosition + displayImageDuration;
-
-  // animateImages(
-  //   index,
-  //   animationDurationOfCombos,
-  //   startingPosition,
-  //   endingPosition
-  // );
-  // animateTextOverlays(endingPosition, animationDurationOfCombos, index);
-
-  if (index === productsLength - 1) {
-    // RESET ANIMATIONS TO INITIAL STATE
-    // clip.addIncident(
-    //   opacity({
-    //     selector: `svg`,
-    //     from: 1,
-    //     to: 0,
-    //     duration: 700,
-    //     easing: "easeInOutCubic",
-    //   }),
-    //   endingPosition + animationDurationOfCombos
-    // );
-    // clip.addIncident(
-    //   clipPath({
-    //     selector: `.horizontal-line`,
-    //     from: "inset(0% 0% 0% 0%)",
-    //     to: "inset(0% 100% 0% 0%)",
-    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-    //     easing: "easeInOutCubic",
-    //   }),
-    //   endingPosition + animationDurationOfCombos
-    // );
-    // clip.addIncident(
-    //   clipPath({
-    //     selector: `.title-overlay, .subtitle-overlay`,
-    //     from: "inset(100% 0% 0% 0%)",
-    //     to: "inset(0% 0% 0% 0%)",
-    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-    //     easing: "easeInOutCubic",
-    //   }),
-    //   endingPosition
-    // );
-    // clip.addIncident(
-    //   color({
-    //     selector: `.title, .subtitle`,
-    //     from: "var(--fontColor)",
-    //     to: "transparent",
-    //     duration: 50,
-    //   }),
-    //   endingPosition + animationDurationOfCombos
-    // );
-    // clip.addIncident(
-    //   clipPath({
-    //     selector: `.title-overlay, .subtitle-overlay`,
-    //     from: "inset(0% 0% 0% 0%)",
-    //     to: "inset(0% 100% 0% 0%)",
-    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-    //     easing: "easeInOutCubic",
-    //   }),
-    //   endingPosition + animationDurationOfStartingTextOverlaysAndHorizontalLine
-    // );
-    // clip.addIncident(
-    //   clipPath({
-    //     selector: `.cta`,
-    //     from: "inset(0% 0% 0% 0%)",
-    //     to: "inset(0% 100% 0% 0%)",
-    //     duration: animationDurationOfStartingTextOverlaysAndHorizontalLine,
-    //     easing: "easeInOutCubic",
-    //   }),
-    //   endingPosition + animationDurationOfCombos
-    // );
-  }
-});
+animateText(animationDurationOfCircles);
+animateProductImages(animationDurationOfCircles);
 
 export default renderDonkeyclip({ clipId: pkg.id, initParams, clip });
