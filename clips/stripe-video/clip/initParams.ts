4b3212e75fa8aaf6b45ddd0259c398d066197b4a
Theofilos Papadopoulos <priestont@gmail.com>
2024-07-23 14:55:34 +0300

    feat add files

--- clips/stripe-video/clip/initParams.ts
+++ clips/stripe-video/clip/initParams.ts
@@ -2,46 +2,10 @@ export default [
   {
     name: "Blue medium",
     value: {
+      videoUrl:
+        "https://donkeyclip.github.io/clips/clips/stripe-video/assets/background.mp4",
       firstSlideTitle: "Preview the template",
       secondSlideTitle: "Use to Deliver Personalized Content",
-      users: [
-        {
-          name: "Alice",
-          intrest: "shoes",
-          color: "191, 89, 255",
-          product: {
-            title: "Hike 4",
-            description: "New fashion Shoes",
-            price: "300$",
-            image:
-              "https://donkeyclip.github.io/clips/clips/landing-page-v2/images/shoes.jpg",
-          },
-        },
-        {
-          name: "Bob",
-          intrest: "tech",
-          color: "62, 222, 255",
-          product: {
-            title: "Kgbm",
-            description: "New headphones",
-            price: "200$",
-            image:
-              "https://donkeyclip.github.io/clips/clips/landing-page-v2/images/tech.png",
-          },
-        },
-        {
-          name: "Little Timmy",
-          intrest: "bikes",
-          color: "237, 231, 92",
-          product: {
-            title: "Kubel",
-            description: "Bes bike for kids",
-            price: "250$",
-            image:
-              "https://donkeyclip.github.io/clips/clips/landing-page-v2/images/bike.jpg",
-          },
-        },
-      ],
     },
   },
 ];
