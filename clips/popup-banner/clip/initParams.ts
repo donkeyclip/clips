6d408073897f3674eb802bc7ae1a0efa078b8b5d
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2025-05-20 10:41:20 +0300

    feat: initialize popup banner

---
+++ clips/popup-banner/clip/initParams.ts
@@ -0,0 +1,40 @@
+export default [
+  {
+    name: "Blue medium",
+    value: {
+      products: [
+        {
+          image:
+            "https://donkeyclip.github.io/clips/clips/popup-banner/images/image-1.png",
+          description:
+            "T-shirt navy blue T-shirt Navy blue lafj asfpkj asfjl aflkl aklk lafasdadad",
+          oldPrice: "3500$",
+          newPrice: "2000$",
+        },
+        {
+          image:
+            "https://donkeyclip.github.io/clips/clips/popup-banner/images/image-2.png",
+          description:
+            "T-shirt navy blue T-shirt Navy blue lafj asfpkj asfjl aflkl aklk lafasdadad",
+          oldPrice: "3500$",
+          newPrice: "2000$",
+        },
+        {
+          image:
+            "https://donkeyclip.github.io/clips/clips/popup-banner/images/image-3.png",
+          description:
+            "T-shirt navy blue T-shirt Navy blue lafj asfpkj asfjl aflkl aklk lafasdadad cheduhfuio  fuheife f frirjfoifj e",
+          oldPrice: "3500$",
+          newPrice: "2000$",
+        },
+      ],
+      title:
+        "Summer Sale On! It is all about t-shirt and trousers to wear today",
+      subtitle:
+        "Save big on seasonal favorite. Save big on seasonal favorite. Save big on season favorite t-shirts and trousers",
+
+      cta: "START SHOPPING",
+      // image cover or contain option
+    },
+  },
+];
